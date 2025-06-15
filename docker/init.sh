c7d3317d232a4408aa3b9464958dd23be345079d
polycarp <pohmsuindraguli@gmail.com>
2025-06-14 22:13:47 -0500

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -12,6 +12,7 @@ docker compose build
 
 echo "Setting up workspace structure ..."
 mkdir -p ../workspace/{current,builds,initramfs,configs}
+mkdir -p ../workspace/initramfs/{init_templates,builds}
 mkdir -p ../sources/{kernels,initramfs}
 mkdir -p ../patches/{examples}
 mkdir -p ../tools/{scripts}
